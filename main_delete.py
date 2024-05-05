93c4c10611839afa2ccefe547a9c0c9613aa992f
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-05 15:59:01 -0700

    Testing the delete object

--- main_delete.py
+++ main_delete.py
@@ -2,7 +2,35 @@
 """ Test delete feature
 """
 from models.engine.file_storage import FileStorage
-from models.state import State
+from models.brand import Brand
 
-fs = FileStorage
+fs = FileStorage()
 
+# all Brands
+all_brands = fs.all(Brand)
+print("All Brands: {}".format(len(all_brands.keys())))
+for brand_key in all_brands.keys():
+    print(all_brands[brand_key])
+
+# Create a new brand
+new_brand = Brand()
+new_brand.name = 'Habit'
+fs.new(new_brand)
+fs.save()
+print("New Brand: {}".format(new_brand))
+
+# All Brands
+all_brands = fs.all(Brand)
+print("All Brands: {}".format(len(all_brands.keys())))
+for brand_key in all_brands.keys():
+    print(all_brands[brand_key])
+
+# Delete the new Brand
+fs.delete(new_brand)
+
+# All Brands
+all_brands = fs.all(Brand)
+print("All Brands: {}".format(len(all_brands.keys())))
+for brand_key in all_brands.keys():
+    print(all_brands[brand_key])
+    
