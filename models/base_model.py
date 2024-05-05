e614c17279dbae52a02ffc6f6ecf0e9e2d6d9c0a
Fikayomi31 <fikayoogundijo@gmail.com>
2024-05-05 23:53:01 +0000

    updating base_model

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,20 @@
 import uuid
 from datetime import datetime
 import models
-import sqlalchemy
+from sqlalchemy import Column, String, Datetime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """Representation of base class"""
+    id = Column(String(60), nullable=False,
+                primary_key=True, unique=True)
+    created_at = Column(Datetime, nullable=False,
+                        default=datetime.datetime.utcnow)
+    updated_at = Column(Datetime, nullable=False,
+                        default=datetime.datetime.utcnow,
+                        onupdate=datetime.datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """initialization of basemodel object from kwargs
@@ -16,17 +25,17 @@ class BaseModel:
             **kwargs: a key pair valje of dict created
         """
         date_format = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
+        if kwargs::wq
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, date_format)
                 if key != "__class__":
                     setattr(self, key, value)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
 
     def __str__(self):
         """Return the str representation"""
@@ -36,6 +45,7 @@ class BaseModel:
     def save(self):
         """Update current time"""
         self.updated_at = datetime.now()
+        storage.new()
         models.storage.save()
 
     def to_dict(self):
@@ -44,4 +54,11 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in new_dict:
+            del new['_sa_instance_state']
         return new_dict
+
+    def delete(self):
+        """Delete the current instance from the storage"""
+        from models import storage
+        storage.delete(self)
